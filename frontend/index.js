9b57c3846694f6ffade4f05f36bf68df1d529d63
junhui4005 <100547902+junhui4005@users.noreply.github.com>
2025-01-03 16:45:05 +0900

    토큰 api 추가
    error: index.js상에서 get items 401 error출력중

--- frontend/index.js
+++ frontend/index.js
@@ -1,61 +1,78 @@
+// 시간 계산하기
 const calcTime = (timestamp) => {
-    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
-    const time = new Date(curTime - timestamp);
-    const hour = time.getHours();
-    const minute = time.getMinutes();
-    const second = time.getSeconds();
-  
-    if (hour > 0) return `${hour}시간 전`;
-    else if (minute > 0) return `${minute}분 전`;
-    else if (second > 0) return `${second}초 전`;
-    else return "방금 전";
-  };
-  
-  const renderData = (data) => {
-    const main = document.querySelector("main");
-    data.reverse().forEach(async (obj) => {
-      const div = document.createElement("div");
-      div.className = "item-list";
-  
-      const imgDiv = document.createElement("div");
-      imgDiv.className = "item-list__img";
-  
-      const img = document.createElement("img");
-      const res = await fetch(`/images/${obj.id}`);
-      const blob = await res.blob();
-      const url = URL.createObjectURL(blob);
-      img.src = url;
-  
-      const infoDiv = document.createElement("div");
-      infoDiv.className = "item-list__info";
-  
-      const infoTitleDiv = document.createElement("div");
-      infoTitleDiv.className = "item-list__info-title";
-      infoTitleDiv.innerText = obj.title;
-  
-      const infoMetaDiv = document.createElement("div");
-      infoMetaDiv.className = "item-list__info-meta";
-      infoMetaDiv.innerText = obj.place + "" + calcTime(obj.insertAt);
-  
-      const infoPriceDiv = document.createElement("div");
-      infoPriceDiv.className = "item-list__info-price";
-      infoPriceDiv.innerText = obj.price;
-  
-      imgDiv.appendChild(img);
-      infoDiv.appendChild(infoTitleDiv);
-      infoDiv.appendChild(infoMetaDiv);
-      infoDiv.appendChild(infoPriceDiv);
-      div.appendChild(infoDiv);
-      div.appendChild(imgDiv);
-      div.appendChild(infoDiv);
-      main.appendChild(div);
-    });
-  };
-  
-  const fetchList = async () => {
-    const res = await fetch("items");
-    const data = await res.json();
-    renderData(data);
-  };
-  
-  fetchList();
+  // 한국시간 UTC+9 : getTime()
+  const curTime = new Date().getTime() - 9 * 60 * 60 * 1000; // 세계시간으로 변경
+  const time = new Date(curTime - timestamp);
+  const hour = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  if (hour > 0) return `${hour}시간 전`;
+  else if (minutes > 0) return `${minutes}분 전`;
+  else if (seconds > 0) return `${seconds}초 전`;
+  else return "방금전";
+};
+
+// 판매되는 이미지 출력하기
+const renderDate = (data) => {
+  const main = document.querySelector("main");
+
+  // reverse() 배열 뒤집기
+  data.reverse().forEach(async (obj) => {
+    const div = document.createElement("div");
+    div.className = "item-list";
+
+    const infoDiv = document.createElement("div");
+    infoDiv.className = "item-list__info";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "item-list__img";
+
+    const img = document.createElement("img");
+    const res = await fetch(`/image/${obj.id}`);
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob); // url 생성
+    img.src = url;
+
+    const itemListInfoTitleDiv = document.createElement("div");
+    itemListInfoTitleDiv.className = "item-list__info-title";
+    itemListInfoTitleDiv.innerText = obj.title;
+
+    const itemListInfoMetaDiv = document.createElement("div");
+    itemListInfoMetaDiv.className = "item-list__info-meta";
+    itemListInfoMetaDiv.innerText = obj.place + " " + calcTime(obj.insertAt);
+
+    const itemListInfoPriceDiv = document.createElement("div");
+    itemListInfoPriceDiv.className = "item-list__info-price";
+    itemListInfoPriceDiv.innerText = obj.price;
+
+    imgDiv.appendChild(img);
+    infoDiv.appendChild(itemListInfoTitleDiv);
+    infoDiv.appendChild(itemListInfoMetaDiv);
+    infoDiv.appendChild(itemListInfoPriceDiv);
+    div.appendChild(imgDiv);
+    div.appendChild(infoDiv);
+    main.appendChild(div);
+  });
+};
+
+// 서버로부터 데이터받아오기
+const accessToken = window.localStorage.getItem("token");
+const fetchList = async () => {
+  const res = await fetch("/items", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  console.log(res.status);
+  if (res.status == 401) {
+    alert("로그인이 필요합니다!");
+    window.location.pathname = "/login.html";
+    return;
+  }
+
+  const data = await res.json();
+  renderDate(data);
+};
+
+fetchList();
